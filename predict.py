73055f5f22faad358ad6bc481dd5b1afeebefbcc
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-06-23 21:47:04 -0400

    Add files via upload

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import pickle
 import cv2
+import numpy as np
 
 # Load pickle file containing the test set
 with open("test_data.pkl", "rb") as f:
@@ -10,8 +11,6 @@ with open("test_data.pkl", "rb") as f:
 X_test = test_set['X_test']
 y_test = test_set['y_test']
 
-print(X_test[4].shape)
-print(y_test[4].shape)
 # Dictionary of traffic sign labels
 label_dict = {
     'stop sign': 0,
@@ -28,14 +27,37 @@ label_dict = {
 # Code sourced from: https://dontrepeatyourself.org/post/save-and-load-models-with-tensorflow/
 cnn = tf.keras.models.load_model('traffic-sign-cnn')
 
-# Reshape the input data to match the expected shape
-X_test_reshaped = X_test.reshape(-1, 224, 224, 3)
 
-# Run the first 16 elements of the test set through the CNN
-for i in range(16):
-    prediction = cnn.predict(X_test_reshaped[i])
-    predicted_label = label_dict[prediction.argmax()]
-    actual_label = label_dict[y_test[i]]
-    print("Predicted label:", predicted_label)
-    print("Actual label:", actual_label)
-    print()
+# Plot images from X_test with the CNN's predictions and their actual values
+num_images = 8*8  # Number of images to plot (in this case, 6 rows and 6 columns)
+cols = 8  # Number of columns in the grid
+rows = (num_images + cols - 1) // cols  # Calculate the number of rows based on the number of images
+
+fig, axes = plt.subplots(rows, cols, figsize=(45, 45))  # Create a figure with subplots
+
+for i, ax in enumerate(axes.flatten()):
+    if i < num_images:
+        img = X_test[i]  # Get the i-th image array
+        ax.imshow(img)  # Plot the image
+        ax.axis('off')  # Turn off the axis
+
+        # Get the CNN's prediction for the image
+        y_pred = cnn.predict(img[np.newaxis, ...])  # Add an additional dimension for batch
+
+        # Get the predicted and actual labels
+        predicted_class = np.argmax(y_pred)  # Get the index of the predicted class
+        y_true_index = np.argmax(y_test[i])  # Get the index of the actual class
+        y_true_label = [k for k, v in label_dict.items() if v == y_true_index][0]  # Retrieve the label from the label_dict
+        predicted_label = [k for k, v in label_dict.items() if v == predicted_class][0]  # Retrieve the predicted label
+
+        # Combine the predicted and actual labels in the title
+        title = 'Prediction: ' + predicted_label + '\nActual: ' + y_true_label  # Combine the predicted and actual labels
+        if not (predicted_label == y_true_label):
+            ax.set_title(title, color='red')
+        else:
+            ax.set_title(title, color='black')
+    else:
+        ax.axis('off')  # Hide empty subplots
+
+plt.tight_layout()  # Adjust the spacing between subplots
+plt.show()
